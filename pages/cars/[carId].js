00b0661e405230205f9d2bef2ea21c281ddb3e88
miss-f-g <fatemeghafari98@gmail.com>
2023-02-08 14:59:03 +0330

    ep33 - add style to CarDetails

--- pages/cars/[carId].js
+++ pages/cars/[carId].js
@@ -1,15 +1,13 @@
+import CarDetails from "@/components/templates/CarDetails";
 import carsData from "@/data/carsData";
 import { useRouter } from "next/router";
-import React from "react";
 
 const CarDetail = () => {
   const router = useRouter();
   const { carId } = router.query;
-
   const carDetails = carsData[carId - 1];
-  console.log(carDetails);
 
-  return <div>CarDetail</div>;
+  return <CarDetails {...carDetails} />;
 };
 
 export default CarDetail;
