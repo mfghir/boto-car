f0aec08ee7fbbfd1d3a14a57fe3daefef76be819
miss-f-g <fatemeghafari98@gmail.com>
2023-02-09 13:17:22 +0330

    ep38 - write filter for price

--- components/modules/SearchBar.js
+++ components/modules/SearchBar.js
@@ -1,13 +1,36 @@
+import { useRouter } from "next/router";
+import { useState } from "react";
 import styles from "./SearchBar.module.css";
 
 const SearchBar = () => {
+  const router = useRouter();
+
+  const [min, setMin] = useState("");
+  const [max, setMax] = useState("");
+
+  const searchHandler = () => {
+    if (min && max) {
+      router.push(`/filter/${min}/${max}`);
+    } else {
+      alert("Please Enter minimum and maximum price!");
+    }
+  };
+
   return (
     <div className={styles.container}>
       <div>
-        <input placeholder="Inter min-price" />
-        <input placeholder="Inter max-price" />
+        <input
+          placeholder="Inter min-price"
+          value={min}
+          onChange={(e) => setMin(e.target.value)}
+        />
+        <input
+          placeholder="Inter max-price"
+          value={max}
+          onChange={(e) => setMax(e.target.value)}
+        />
       </div>
-      <button>Search</button>
+      <button onClick={searchHandler}>Search</button>
     </div>
   );
 };
